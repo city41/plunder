18fcf7e8ee0afc64ad3083ca2eb98b006b4f9356
Matt Greer <matt.e.greer@gmail.com>
2013-07-18 18:43:39 -0600

    main example now 'hand written' JS instead of compiled coffee

--- js/plunder/main-example.js
+++ js/plunder/main-example.js
@@ -2,64 +2,61 @@
   var Timeline = Plunder.Timeline, U = Plunder.Util;
 
   window.raf = window.requestAnimationFrame
-	|| window.mozRequestAnimationFrame
-	|| window.webkitRequestAnimationFrame
-	|| window.msRequestAnimationFrame;
+    || window.mozRequestAnimationFrame
+    || window.webkitRequestAnimationFrame
+    || window.msRequestAnimationFrame;
 
   var Entity, context, entity, lastTimestamp, tl, update, button, playing = true;
 
-  Entity = (function() {
-    function Entity() {
+  Entity = function() {
+    this.anis = [];
+    this.x = 10;
+    this.y = 10;
+    this.alpha = 1;
+    this.color = [0, 0, 0, 0];
+  };
+
+  Entity.prototype = {
+    addPlunderAnimation: function(ani) {
+      this.anis.push(ani);
+    },
+
+    clearPlunderAnimations: function() {
       this.anis = [];
-      this.x = 10;
-      this.y = 10;
-      this.alpha = 1;
-      this.color = [0, 0, 0, 0];
-    }
+    },
 
-    Entity.prototype.addPlunderAnimation = function(ani) {
-      return this.anis.push(ani);
-    };
-
-    Entity.prototype.clearPlunderAnimations = function() {
-      return this.anis = [];
-    };
-
-    Entity.prototype.update = function(delta) {
-      var ani, _i, _len, _ref, _results;
-      _ref = this.anis;
-      _results = [];
-      for (_i = 0, _len = _ref.length; _i < _len; _i++) {
-        ani = _ref[_i];
-        _results.push(ani.update(delta));
+    update: function(delta) {
+      for(var i = 0, l = this.anis.length; i < l; ++i) {
+        this.anis[i].update(delta);
       }
-      return _results;
-    };
+    },
 
-    Entity.prototype.draw = function(context) {
+    draw: function(context) {
       context.save();
       context.fillStyle = "rgba(" + (this.color[0] | 0) + ", " + (this.color[1] | 0) + ", " + (this.color[2] | 0) + ", " + this.color[3] + ")";
       context.translate(this.x, this.y);
+
       if (this.scale != null) {
         context.scale(this.scale, this.scale);
       }
+
       if (this.angle) {
         context.rotate(U.degreesToRadians(this.angle));
       }
+
       context.fillRect(-5, -5, 10, 10);
-      return context.restore();
-    };
+      context.restore();
+    }
+  };
 
-    return Entity;
-
-  })();
   entity = new Entity();
   tl = new Timeline(entity);
+
   tl.forever(function(tl) {
     var t = tl.together( { duration: 2000 }, function(tl) {
       tl.rotate({
-	from: 0,
-	to: 360
+        from: 0,
+        to: 360
       });
       tl.color({
         from: [255, 0, 0, 1],
@@ -69,7 +66,7 @@
         from: 1,
         to: 10
       });
-      return tl.move({
+      tl.move({
         from: {
           x: 10,
           y: 10
@@ -83,11 +80,11 @@
     tl.wait(1000);
     tl.reverse(t);
   });
+
   context = document.getElementById('canvas').getContext('2d');
-  button= document.getElementById('play-main-example');
+  button = document.getElementById('play-main-example');
 
-
-  button.onclick = function() {
+  button.addEventListener('click', function() {
     playing = !playing;
 
     if(playing) {
@@ -95,9 +92,10 @@
     } else {
       button.innerHTML = "Play";
     }
-  };
+  });
 
   lastTimestamp = null;
+
   update = function(ts) {
     var delta;
     if (lastTimestamp == null) {
@@ -112,7 +110,8 @@
       context.fillRect(0, 0, context.canvas.width, context.canvas.height);
       entity.draw(context);
     }
-    return window.raf(update);
+    window.raf(update);
   };
-  return update(0);
+
+  update(0);
 })();
